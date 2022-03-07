8d4ac25fd2f280a3664475c2d429f7c8ba441d1c
LoeLavaud <Loe.LAVAUD@cpnv.ch>
2022-03-07 16:25:15 +0100

    install git for windows

--- Bataille_Navalle/cmake-build-debug/cmake_install.cmake
+++ Bataille_Navalle/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/sl4sh/CLionProjects/Bataille_Navalle
+# Install script for directory: C:/Users/sl4sh/OneDrive/Documents/GitHub/Bataille_Navalle/Bataille_Navalle
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/sl4sh/CLionProjects/Bataille_Navalle/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/sl4sh/OneDrive/Documents/GitHub/Bataille_Navalle/Bataille_Navalle/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
